2adf2b44b663ff18564ddc38394389cbfa1094a5
andela-awofadeju <olayemi.awofadeju@andela.com>
2018-09-02 15:01:12 +0100

    Update readme and solid files

---
+++ OOP_SOLID_principles/bad_dependency_inversion_principle.rb
@@ -0,0 +1,25 @@
+class Printer
+  def initialize(data)
+    @data = data
+  end
+
+  def print_pdf
+    PdfFormatter.new.format(@data)
+  end
+
+  def print_html
+    HtmlFormatter.new.format(@data)
+  end
+end
+
+class PdfFormatter
+  def format(data)
+    # format data to Pdf logic
+  end
+end
+
+class HtmlFormatter
+  def format(data)
+    # format data to Html logic
+  end
+end
